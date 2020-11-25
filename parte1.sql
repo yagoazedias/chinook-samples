e40016bc4f8797c2968b9e65826554e53cfcf080
Yago Azedias <yagoazedias@gmail.com>
2020-11-25 20:18:18 -0300

    Update parte1.sql

--- parte1.sql
+++ parte1.sql
@@ -1,20 +1,21 @@
 
 -- 1 Consultar as tabelas de catálogo para listar todos os índices existentes acompanhados
 --  das tabelas e colunas indexadas pelo mesmo.
-SELECT * FROM pg_catalog.pg_indexes WHERE tablename = 'MediaType';
+SELECT tablename, indexname, indexdef FROM pg_indexes;
 
 
 -- INCOMPLETE: Ainda é necessário lidar com uma excessão caso não seja possível deletar o index
 -- 2 Criar usando a linguagem de programação do SGBD escolhido um procedimento que
 -- remova todos os índices de uma tabela informada como parâmetro.
-CREATE OR REPLACE FUNCTION remove_indexes_by_table_name (table_name text) RETURNS VOID AS $$
+CREATE OR REPLACE FUNCTION remove_indexes_by_table_name (table_name text) RETURNS INTEGER AS $$
     DECLARE
         r text;
     BEGIN
         FOR r IN
             SELECT indexname FROM pg_indexes WHERE tablename = table_name
         LOOP
-            EXECUTE 'ALTER TABLE "' || table_name || '" DROP CONSTRAINT "' || r || '" CASCADE';
+            EXECUTE 'DROP INDEX IF EXISTS ' || r;
         END LOOP;
+        RETURN 1;
     END
 $$ LANGUAGE plpgsql;

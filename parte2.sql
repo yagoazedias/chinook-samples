c3064c64f8e37ca9ae45168ee24a4c5c2bc1e31d
Yago Azedias <yagoazedias@gmail.com>
2020-11-28 22:57:20 -0300

    Update parte2.sql

--- parte2.sql
+++ parte2.sql
@@ -5,7 +5,8 @@
 -- registros de InvoiceLine relacionados a um Invoice. Implementar uma solução que
 -- garanta a integridade dessa regra.
 
-CREATE OR REPLACE FUNCTION valid_invoice_price_update() RETURNS trigger as $check_world_change$
+-- PASSO 1: Trigger para impedir atualizações de preço
+CREATE OR REPLACE FUNCTION valid_invoice_price_update() RETURNS trigger as $valid_invoice_price_update$
 DECLARE expected_value numeric(10,2);
 BEGIN
     expected_value := (
@@ -19,7 +20,24 @@ BEGIN
 
     RETURN NEW;
 END
-$check_world_change$ LANGUAGE plpgsql;
+$valid_invoice_price_update$ LANGUAGE plpgsql;
 
 CREATE TRIGGER valid_invoice_price_update BEFORE UPDATE ON "Invoice"
   FOR EACH ROW EXECUTE PROCEDURE valid_invoice_price_update();
+
+-- PASSO 2: Trigger para atualizar total na tabela invoice ao inserir uma nova linha na tabela InvoiceLine
+CREATE OR REPLACE FUNCTION update_invoice_price_on_insert_invoice_line() RETURNS trigger as $update_invoice_price_on_insert_invoice_line$
+DECLARE old_total numeric(10,2);
+DECLARE new_total numeric(10,2);
+BEGIN
+    old_total := (select sum("UnitPrice" * "Quantity") from "InvoiceLine" where "InvoiceId" = 10);
+    new_total := old_total + (NEW."UnitPrice" * NEW."Quantity");
+
+    UPDATE "Invoice" SET "Total" = new_total WHERE "InvoiceId" = NEW."InvoiceId";
+
+    RETURN NEW;
+END
+$update_invoice_price_on_insert_invoice_line$ LANGUAGE plpgsql;
+
+CREATE TRIGGER update_invoice_price_on_insert_invoice_line BEFORE INSERT ON "InvoiceLine"
+  FOR EACH ROW EXECUTE PROCEDURE update_invoice_price_on_insert_invoice_line();
